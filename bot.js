1076d6a6f86a1e6d9881320167581f4ce7bcae28
Juan <alonsojuan2007@gmail.com>
2023-07-12 14:56:03 -0300

    7.5

--- bot.js
+++ bot.js
@@ -19,10 +19,8 @@ const T = new Twit(twitterConfig);
 // Middleware para analizar el cuerpo de la solicitud como JSON
 app.use(express.json());
 
-// Ruta para servir el archivo HTML de la página de confesiones
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+// Ruta para servir los archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta para recibir confesiones
 app.post('/confessions', (req, res) => {
